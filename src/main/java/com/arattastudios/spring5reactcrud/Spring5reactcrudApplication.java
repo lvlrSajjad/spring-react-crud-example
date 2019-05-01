07069a1e87e05e7bdacf0aa0997ebb1f4ebcceb4
Sajjad Asadi <lvlr.xaus@gmail.com>
2019-05-01 23:59:27 +0430

    first commit

---
+++ src/main/java/com/arattastudios/spring5reactcrud/Spring5reactcrudApplication.java
@@ -0,0 +1,13 @@
+package com.arattastudios.spring5reactcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5reactcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5reactcrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arattastudios/spring5reactcrud/Spring5reactcrudApplicationTests.java
@@ -0,0 +1,16 @@
+package com.arattastudios.spring5reactcrud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring5reactcrudApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
